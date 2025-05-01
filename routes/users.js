2bfd467afe843014bbb2af76e8af4462e1e64842
Priyanshu <priyanshu.sde@gmail.com>
2025-05-02 02:14:06 +0530

    Empty commit

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,54 @@
-import express from 'express';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+const prisma = new PrismaClient();
 const router = express.Router();
 
-router.post('/signup', async (req,res) => {
-    const {name,username,password} = req.body;
-    await Users.create({
-        name,
-        email,
-        password
-    });
-    res.json({message: "You are signed up"});
-})
+const JWT_SECRET = env("JWT_SECRET");
 
-router.post('/signin', async (req,res) => {
-    const {username, password} = req.body;
-    const user = await Users.findOne({email});
-    if( user && user.password === password){
-        const token = generateToken(user._id);
-        res.json({
-        message: 'You are signed in',token
-        })
-    }
-    else{
-        res.status(403).json({message:'incorrect credential'});
+router.post("/signup", async (req, res) => {
+    try {
+        const { name, username, password } = req.body;
+
+        const existingUser = await prisma.user.findUnique({
+            where: { username },
+        });
+        if (existingUser) {
+            return res.json({
+                message: "Username already taken",
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                username,
+                password: hashedPassword,
+            },
+        });
+
+        res.json({ message: "You are signed up", user });
+    } catch (e) {
+        console.error(error);
+        res.json({ message: "Server error" });
     }
 });
- export default router;
+
+router.post("/signin", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await prisma.user.findUnique({
+        where: {username},
+    });
+
+    if(!user){
+        return res.json({message: "Username not found"});
+    }
+    const isMatched = await bcrypt.compare(password, user.password);
+    if(!isMatched){
+        return res.json({message: "Invalid credentials"});
+    }
+});
+export default router;
